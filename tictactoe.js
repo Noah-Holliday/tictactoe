8616f26a92897295aaadc90ecbbfdc46f0d3925d
Noah Holliday <nextechcatapult@Jordans-iPad.localdomain>
2018-07-02 14:54:28 -0400

    working tictac

--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,8 @@
 var currentPlayer = "X";
 var nextPlayer = "O";
-
-var playerXSelections = new Array();
-var playerOSelections = new Array();
+var playerSelections = [];
+var playerXSelections = [];
+var playerOSelections = [];
 
 const winningCombinations = [
     [1, 2, 3],
@@ -13,32 +13,76 @@ const winningCombinations = [
     [3, 6, 9],
     [1, 5, 9],
     [3, 5, 7]
-  ]
+]
 
-  handleClick = function(event) {
+handleClick = function (event) {
+    
     var cell = event.target;
-  
-    cell.innerHTML = currentPlayer;
-  
-    if(currentPlayer === "X" ) {
-      playerSelections = playerXSelections;
-      nextPlayer = "O";
-    } else {
-      playerSelections = playerOSelections;
-      nextPlayer = "X";
-    }
-  
-    playerSelections.push(parseInt(cell.id));
-  
-    // Swap players
-    currentPlayer = nextPlayer;
-
-    var cell = event.target
     console.log(cell.id);
-  }
-  
-  var cells = document.querySelectorAll("td");
-  
-  for(var i = 0; i < cells.length; i++) {
+    
+    cell.innerHTML = currentPlayer;
+    
+    if (currentPlayer === "X") {
+        playerSelections = playerXSelections;
+        nextPlayer = "O";
+    } else {
+        playerSelections = playerOSelections;
+        nextPlayer = "X";
+    }
+    
+    console.log("playerSelections", playerSelections);
+    console.log("playerXSelections", playerXSelections);
+    console.log("playerOSelections", playerOSelections);
+
+    playerSelections.push(parseInt(cell.id));
+    // Swap players
+
+
+    let checklol = checkWinner();
+    if (checklol==true) {
+        alert("Player " + currentPlayer + " wins!")
+        resetGame();
+    }
+
+    if (checkDraw()) {
+        alert("Draw!");
+        resetGame();
+    }
+
+    currentPlayer = nextPlayer;
+}
+
+var cells = document.querySelectorAll("td");
+
+for (var i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', handleClick)
-  }
+}
+
+function checkWinner() {
+    // Check if player has all values of each combination
+    for (let check of winningCombinations) {
+        let matches = 0;
+        console.log("check", check);
+        for (let check1 of playerSelections) {
+            console.log("playerSelections", playerSelections);
+            if (check.includes(check1)) {
+                matches++
+            }
+            if (matches == 3) {
+                return true
+            }
+        }
+    }
+    console.log();
+}
+function checkDraw() {
+    return playerOSelections.length + playerXSelections.length >= cells.length
+}
+
+function resetGame() {
+    playerXSelections = [];
+    playerOSelections = [];
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = ""
+    }
+}
